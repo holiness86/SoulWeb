0728831af39d3ad53d20bb6a017ee87f1a2d4627
holiness <arshyarezadost@gmail.com>
2026-08-08 08:42:29 +0330

    up on Host4

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ const Category  = require('./models/category')
 //  DB + SERVER START
 // ─────────────────────────────────────────
 // const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/SoulWeb'
-const DB_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/SoulWeb'
+const DB_URL = 'mongodb://admin:pM8U6IsnbQBsnGspAg1f@soulwebdb-ttk-service:27017/admin' || 'mongodb://localhost:27017/SoulWeb'
 const PORT   = process.env.PORT   || 3000
 
 mongoose.connect(DB_URL)

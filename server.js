3c07b0dc74ac4909b6dd638a8beaa51256ca479c
holiness <arshyarezadost@gmail.com>
2026-08-08 08:30:08 +0330

    up on Host2

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const puppeteer  = require('puppeteer-core')
 const ejs        = require('ejs')
 const session    = require('express-session');
 const moment     = require('jalali-moment');
+require('dotenv').config();
 
 const app = express()
 
@@ -109,7 +110,7 @@ const Category  = require('./models/category')
 //  DB + SERVER START
 // ─────────────────────────────────────────
 // const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/SoulWeb'
-const DB_URL = 'mongodb://admin:pM8U6IsnbQBsnGspAg1f@soulwebdb-ttk-service:27017/admin' || 'mongodb://localhost:27017/SoulWeb'
+const DB_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/SoulWeb'
 const PORT   = process.env.PORT   || 3000
 
 mongoose.connect(DB_URL)

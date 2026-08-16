397c689624aca3afb1779c60e48fd719227b65bd
holiness <arshyarezadost@gmail.com>
2026-08-16 09:15:50 +0330

    fixing index page to get data as DB

--- server.js
+++ server.js
@@ -110,7 +110,8 @@ const Category  = require('./models/category')
 //  DB + SERVER START
 // ─────────────────────────────────────────
 // const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/SoulWeb'
-const DB_URL = 'mongodb://admin:pM8U6IsnbQBsnGspAg1f@soulwebdb-ttk-service:27017/admin' || 'mongodb://localhost:27017/SoulWeb'
+const DB_URL = 'mongodb://admin:pM8U6IsnbQBsnGspAg1f@soulwebdb-ttk-service:27017/admin'
+// const DB_URL = 'mongodb://localhost:27017/SoulWeb'
 const PORT   = 3000
 
 mongoose.connect(DB_URL)
